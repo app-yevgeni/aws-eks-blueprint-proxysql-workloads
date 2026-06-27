f5a21d6b9a990fecd9c09bd405f6dce16f67ba2a
app-yevgeni <23049337+app-yevgeni@users.noreply.github.com>
2026-06-27 18:29:13 +0300

    Initial commit

---
+++ modules/kong/variables.tf
@@ -0,0 +1,23 @@
+variable "name" {
+  description = "The name of the service/ingress"
+  type        = string
+  default     = "httpd-server"
+}
+
+variable "namespace" {
+  description = "Namespace where the ingress and service are deployed"
+  type        = string
+  default     = "default"
+}
+
+variable "host" {
+  description = "Hostname for ingress"
+  type        = string
+  default     = "app.crypterio.co"
+}
+
+variable "service_port" {
+  description = "Port of the backend service"
+  type        = number
+  default     = "8080"
+}
